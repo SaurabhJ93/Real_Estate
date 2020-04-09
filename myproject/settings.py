57efcceb40c565201f371057b9d8b9581626a1b7
Saurabh Jaiswal <saurabhjaiswal93@gmail.com>
2020-04-09 15:16:57 -0400

    Adding listings model

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'real_estate',
+        'USER': 'postgres',
+        'PASSWORD': 'welcome@123',
+        'HOST': 'localhost',
     }
 }
 
