1e55af65a75686bd33f8e2485bcad3b1c2873fcd
Saurabh Jaiswal <saurabhjaiswal93@gmail.com>
2020-04-25 15:29:02 -0400

    Contacts app (#7)
    * Contact app and forms
    
    * Completed contacts app
    
    * changed logo and phone

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from contacts.models import Contact
 
 def register(request):
     if request.method == 'POST':
@@ -55,4 +56,12 @@ def logouthere(request):
         return redirect('index')
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+
+    data = {
+        'contacts': user_contacts
+    }
+
+    return render(request, 'accounts/dashboard.html', data)
+    
